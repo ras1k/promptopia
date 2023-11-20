b6182434711a8056bc603559fc02b3810b0b14e7
Rasik Abdullah <rasikabdullah@gmail.com>
2023-11-20 20:54:32 +0600

    added components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Nav from "@components/Nav";
 import "@styles/globals.css";
 
 export const metadata = {
@@ -9,13 +10,14 @@ const RootLayout = ({ children }) => (
   <html lang='en'>
     <body>
 
-        <div className='main'>
-          <div className='gradient' />
-        </div>
+      <div className='main'>
+        <div className='gradient' />
+      </div>
 
-        <main className='app'>
-          {children}
-        </main>
+      <main className='app'>
+        <Nav />
+        {children}
+      </main>
     </body>
   </html>
 );
